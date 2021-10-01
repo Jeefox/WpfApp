b41b70e1dbe9a1dae2f73b66b6942f34aa81b2bb
Jeefox <G89202700802@yandex.ru>
2021-10-01 20:22:30 +0300

    Добавьте файлы проекта.

---
+++ Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButClick1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page2());
+        }
+    }
+}
---
+++ Page2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class ViewModel : ViewModelBase
+
+    {
+        TestTaskEntities testTaskDBContext;
+        public ViewModel()
+        {
+            if (IsInDesignMode)
+            {
+                Valutes = new ObservableCollection<Valute>();
+            }
+            else
+            {
+                testTaskDBContext = new TestTaskEntities();
+
+                testTaskDBContext.Valute.Load();
+                Valutes = testTaskDBContext.Valute.Local;
+            }          
+        }
+        public ObservableCollection<Valute> Valutes
+        {
+            get => GetValue<ObservableCollection<Valute>>();
+            private set => SetValue(value);
+        }
+        
+    }
+}
