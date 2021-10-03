d83838bf71ff5d2097895dc98ac239b9ed201aaf
Jeefox <G89202700802@yandex.ru>
2021-10-04 00:01:21 +0300

    что то работает

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using WpfApp2.Models;
 using System.Xml;
+using System.Collections.Generic;
 
 namespace WpfApp2
 {
@@ -37,25 +38,16 @@ namespace WpfApp2
         {
             string URLString = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=02/03/2002";
             XmlReader reader = new XmlTextReader(URLString);
-
-
+            List<object> masObj = new List<object>();
             while (reader.Read())
             {
-                switch (reader.Name)
+               for (int i = 0; i<6; i++)
                 {
-                    case "Id": // The node is an element.                        
-                        string str = reader.Value;
-                        while (reader.MoveToNextAttribute()) // Read the attributes.
-                            str = reader.Value;
-                        break;
-                    case "NumCode": //Display the text in each element.
-                        str = reader.Value;
-                        break;
-                    case "ChaCode": //Display the end of the element.
-                        str = reader.Name;
-                        break;
+                    masObj[i] = reader.Value;
                 }
+                db.Valutes.Add(masObj);
             }
+
         }
     }
 }
