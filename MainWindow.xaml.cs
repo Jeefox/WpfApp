7b4f03d78be6c8c9355e1d50e2d1cb5c0bfa448b
Jeefox <G89202700802@yandex.ru>
2021-10-04 17:16:49 +0300

    #3

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,104 +16,111 @@ namespace WpfApp2
         {
             InitializeComponent();
             MainFrame.Content = new Page1();
-            AddDataToTable();
+           
         }
 
         private void ButtClick1(object sender, RoutedEventArgs e)
         {
-           /* using (ValuteContext db = new ValuteContext())
-            {
-                Valute tableRow = new Valute { };
-                db.Valutes.Add(tableRow);
-                db.SaveChanges();
-                var Word = db.Valutes;
-                db.Valutes.Add(masObj);
-                foreach (Valute v in Word)
-                {
-                    MessageBox.Show($"{v.Id}, {v.NumCode}");
-                }
-            }*/
+            /* using (ValuteContext db = new ValuteContext())
+             {
+                 Valute tableRow = new Valute { };
+                 db.Valutes.Add(tableRow);
+                 db.SaveChanges();
+                 var Word = db.Valutes;
+                 db.Valutes.Add(masObj);
+                 foreach (Valute v in Word)
+                 {
+                     MessageBox.Show($"{v.Id}, {v.NumCode}");
+                 }
+             }*/
 
         }
 
-        private void AddDataToTable()
+        public static void AddDataToTable()
         {
-            using (ValuteContext db = new ValuteContext()) { 
-            int count = 0;
-            string URLString = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=04/10/2021";
-            XmlReader reader = new XmlTextReader(URLString);
-
-            List<Valute> masObj = new List<Valute>();
-            Valute z = new Valute();
-            
-            reader.MoveToAttribute("NumCode");                       
-            while (reader.Read())
+            using (ValuteContext db = new ValuteContext())
             {
-            switch (reader.Name) { 
-                        case "Valute":
-                            while (reader.NodeType != XmlNodeType.Text) 
-            {       
-                reader.Read();
-                
-            }
-                            z.Id = reader.Value;
-                            break;
-            case "NumCode":
-            
-            while (reader.NodeType != XmlNodeType.Text) 
-            {       
-                reader.Read();
-                
-            }
-            z.NumCode = (reader.Value);
-            break;
-            case "CharCode":
-                while (reader.NodeType != XmlNodeType.Text) 
-            {       
-                reader.Read();
-                
-            }
-                z.CharCode = (reader.Value);
-            break;
+                int count = 0;
+                string URLString = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=04/10/2021";
+                XmlReader reader = new XmlTextReader(URLString);
 
-            case "Nominal":
-                while (reader.NodeType != XmlNodeType.Text) 
-            {       
-                reader.Read();
-                
-            }
-                z.Nominal = (reader.Value);
-            break;
+                List<Valute> masObj = new List<Valute>();
+                Valute z = new Valute();
+
+                reader.MoveToAttribute("NumCode");
+                while (reader.Read())
+                {
+                    switch (reader.Name)
+                    {
+                        case "Valute":
+                            while (reader.NodeType != XmlNodeType.Text)
+                            {
+                                reader.Read();
+
+                            }
+                            z.Id = reader.Value;
+                            count++;
+                            break;
+                        case "NumCode":
+                            while (reader.NodeType != XmlNodeType.Text)
+                            {
+                                reader.Read();
+
+                            }
+                            z.NumCode = (reader.Value);
+                            count++;
+                            break;
+                        case "CharCode":
+                            while (reader.NodeType != XmlNodeType.Text)
+                            {
+                                reader.Read();
+
+                            }
+                            z.CharCode = (reader.Value);
+                            count++;
+                            break;
+
+                        case "Nominal":
+                            while (reader.NodeType != XmlNodeType.Text)
+                            {
+                                reader.Read();
+
+                            }
+                            z.Nominal = reader.Value;
+                            count++;
+                            break;
 
                         case "Name":
-                while (reader.NodeType != XmlNodeType.Text) 
-            {       
-                reader.Read();
-                
-            }
-                z.Name = (reader.Value);
-            break;
+                            while (reader.NodeType != XmlNodeType.Text)
+                            {
+                                reader.Read();
+
+                            }
+                            z.Name = (reader.Value);
+                            count++;
+                            break;
 
                         case "Value":
-                while (reader.NodeType != XmlNodeType.Text) 
-            {       
-                reader.Read();
-                
-            }
-                z.Value = reader.Value;
-            break;
-            }
-                count++;
-                    if(count == 6)
-                {
-                    masObj.Add(z);
+                            while (reader.NodeType != XmlNodeType.Text)
+                            {
+                                reader.Read();
+
+                            }
+                            z.Value = reader.Value;
+                            count++;
+                            break;
+                    }
+
+                    if (count < 6)
+                    {
+                        masObj.Add(z);
                         count = 0;
-                     z = new Valute();
+                        z = new Valute();
+                    }
                 }
-            }
-            db.Valutes.AddRange(masObj);
+                db.Valutes.AddRange(masObj);
                 db.SaveChanges();
-        }
             }
+        }
     }
 }
